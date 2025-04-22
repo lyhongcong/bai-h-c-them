ad7bf81822082a0b30b6e1183cb20bb47fac43de
lyhongcong <conglhph48542@gmail.com>
2025-04-22 16:20:52 +0700

    bài 0

--- src/helloworld.java
+++ src/helloworld.java
@@ -1,6 +1,14 @@
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("xin chào");
-        System.out.println("I LOVE YOU ");
+//        System.out.println("xin chào");
+//        System.out.println("I LOVE YOU ");
+        /////////
+//        int a = 6;
+//        int b = 9;
+//        int c = a + b;
+//        System.out.println("Sum: " + c);
+        /////////
+        int test = 5 + 9*3 - 10/2; // 27
+        System.out.println("check test : " + test);
     }
 }
